968f9a8c5622a9b50e3a5d37dc2fd4e108d52a94
Zaturrby <rjkorteschiel@gmail.com>
2015-09-21 16:31:20 +0200

    first styling in

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,9 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/simulacrum-logo.svg"/></a>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><button class="btn menu-button">MENU</button></a>
+    <!-- <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a> -->
+
     <nav role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
