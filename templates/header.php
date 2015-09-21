04d2b336afee7cb7e0945062e05aba194bc61b44
Zaturrby <rjkorteschiel@gmail.com>
2015-09-21 20:31:09 +0200

    small change header and add screanshot

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/simulacrum-logo.svg"/></a>
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><button class="btn menu-button">MENU</button></a>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/logozwart.svg"/></a>
+    <a href="<?= esc_url(home_url('/')); ?>"><button class="btn menu-button">MENU</button></a>
     <!-- <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a> -->
 
     <nav role="navigation">
