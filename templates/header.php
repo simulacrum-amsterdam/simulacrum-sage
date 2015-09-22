54b39afc1f734fb24eb06c8f8820d22a8dc9c374
Zaturrby <rjkorteschiel@gmail.com>
2015-09-22 23:14:35 +0200

    better organisation with grid

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,12 @@
-<header class="banner" role="banner">
-  <div class="container header-nav-container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/logozwart-edged.svg"/></a>
-    <a href="<?= esc_url(home_url('/')); ?>"><button class="btn menu-button">MENU</button></a>
-    <!-- <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a> -->
-
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<header class="nav-top">    
+      <a href="<?= esc_url(home_url('/')); ?>"><button class="btn menu-button">MENU</button></a>
 </header>
+
+
+<!-- <nav class="nav-sidebar-off" role="navigation">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        endif;
+        ?>
+      </nav> -->
---
+++ templates/side-nav.php
@@ -0,0 +1,13 @@
+<aside class="side-nav" role="banner">
+    <div class="wrap-fixed">    
+    <!-- this div contains the nav menu, and it out of view on standard sizes -->
+      <div class="header-left">
+        
+      </div>
+     
+      <!-- this div contains the logo and the login button -->
+      <div class="header-right">
+        <a href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/logozwart-edged.svg"/></a>
+      </div>
+    </div>
+</aside>
