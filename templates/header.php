dfb3467df3e837d149ae61fcb690b58502ef1d7c
Zaturrby <rjkorteschiel@gmail.com>
2015-09-22 14:14:10 +0200

    changed some styling

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/logozwart.svg"/></a>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/logozwart-edged.svg"/></a>
     <a href="<?= esc_url(home_url('/')); ?>"><button class="btn menu-button">MENU</button></a>
     <!-- <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a> -->
 
