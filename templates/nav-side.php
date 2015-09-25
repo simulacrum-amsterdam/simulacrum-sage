7b54b7dd4bac718ce0f302d816ebac6ea09188f4
Zaturrby <rjkorteschiel@gmail.com>
2015-09-25 20:36:50 +0200

    small change to nav-side

--- templates/nav-side.php
+++ templates/nav-side.php
@@ -1,9 +1,8 @@
 <aside class="nav-side" role="banner">
-    <div class="nav-top">
-      <p class="menu-button">MENU</p>
-    </div>
-
-    <div class="wrap-fixed">    
+    <div class="wrap-fixed">
+      <div class="nav-top">
+        <p class="menu-button">MENU</p>
+      </div>    
     <!-- this div contains the nav menu, and it out of view on standard sizes -->
       <div class="header-left">
         <nav class="navbar navbar-default" role="navigation">
@@ -14,7 +13,6 @@
 	        ?>
       	</nav>
       </div>
-     
       <!-- this div contains the logo and the login button -->
       <div class="header-right">
         <a href="<?= esc_url(home_url('/')); ?>"><img class="main-logo hidden-xs" src="<?php bloginfo('template_directory'); ?>/dist/images/logozwart-edged.svg"/></a>
