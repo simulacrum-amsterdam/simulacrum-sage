22c6650ded3f0c8b71ac7d0d53b4396c91735613
Zaturrby <rjkorteschiel@gmail.com>
2015-09-25 13:47:51 +0200

    #2 some js style issues choking travis

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,3 +1,3 @@
-jQuery('.menu-button').click(function(){
-	jQuery('.main-wrapper, .nav-side').toggleClass('on');
+jQuery('.menu-button').click(function() {
+  jQuery('.main-wrapper, .nav-side').toggleClass('on');
 });
