d2b444affc91bcd650dcafc65ca535c38b16b6e4
Zaturrby <rjkorteschiel@gmail.com>
2015-09-30 12:37:51 +0200

    branchout blackstyle

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
 	$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );
 ?>
 
-<div class="main-header container-fluid" style="background: url(<?php echo $src[0]; ?> ) center center/100% auto no-repeat;">
+<div class="main-header container-fluid" >
 	<div class="main-heading-container"  >	
 		<h1><?= Titles\title(); ?></h1>
 	</div>
