baf50488fb5539bccba97eb87b89e785d6284d71
Zaturrby <rjkorteschiel@gmail.com>
2015-10-01 19:46:40 +0200

    put the excerpts into grid

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,4 @@
-<article <?php post_class(); ?>>
+<article <?php post_class("excerpt_class"); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
