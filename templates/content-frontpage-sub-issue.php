c2f111215ac7bef224061fd469da612b5a11bfa3
Zaturrby <rjkorteschiel@gmail.com>
2015-10-03 21:46:44 +0200

    fix identation

--- templates/content-frontpage-sub-issue.php
+++ templates/content-frontpage-sub-issue.php
@@ -1,7 +1,7 @@
 <article <?php post_class(); ?>>
-  	<header>
-    	<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-  	</header>
+	<header>
+  	<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+	</header>
 	<div class="entry-summary">
     	<?php echo the_excerpt(); ?>
   </div>
