e485e30144721f72d787e2e825f438ae5003ab43
Zaturrby <rjkorteschiel@gmail.com>
2015-10-03 11:03:19 +0200

    #10 fix to much on one line JS

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,3 +1,4 @@
 jQuery('.menu-button').click(function() {
-  jQuery('.main-wrapper, .nav-side, .footer-wrapper, .menu-button').toggleClass('on');
+	var selectClasses = '.main-wrapper, .nav-side, .footer-wrapper, .menu-button';
+  jQuery(selectorClasses).toggleClass('on');
 });
