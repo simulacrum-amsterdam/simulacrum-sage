1b6ff0c6c2e6eb81f9b54ee699c3597e48c6fd3d
Zaturrby <rjkorteschiel@gmail.com>
2015-10-03 14:59:30 +0200

    #7 small change

--- templates/content-frontpage.php
+++ templates/content-frontpage.php
@@ -9,7 +9,7 @@
 </div>
 <div class="frontpage-issue">
 	<div class="frontpage-current-issue">
-		<h5 class="small-title"> Huidige Nummer: </h5>
+		<h5 class="small-title"> Nieuwste nummer: </h5>
 		<img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 		<h1>OEF!</h1>
 		<p> Dit geweldige nieuwe nummer verkent Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa deserunt adipisci tenetur nostrum a libero, et fugit magnam cum. Sunt odio numquam quas earum beatae quaerat commodi, accusamus ducimus velit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi obcaecati molestiae dignissimos, assumenda nemo fuga tempore enim deleniti consequatur animi fugiat doloremque sit natus ullam adipisci perferendis nam incidunt.</p>
