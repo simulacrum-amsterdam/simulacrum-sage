377520883e94b8b07eb7dcbf7d4d82bcb0a6ada8
Zaturrby <rjkorteschiel@gmail.com>
2015-10-03 21:51:50 +0200

    #8 start on about

--- templates/content-frontpage-about.php
+++ templates/content-frontpage-about.php
@@ -1,5 +1,12 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div>Hi!</div>
+<div class="top-row">
+	<div class="about"></div>
+	<div class="about-topics"></div>
+</div>
+<div class="people-row">
+	<div class="current-staff"></div>
+	<div class="retired-staff"></div>
+</div>
 
 
