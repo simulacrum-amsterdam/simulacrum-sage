01b4c1a71d013aae649844dc2762cf2ecd19e90c
Zaturrby <rjkorteschiel@gmail.com>
2015-10-03 20:59:34 +0200

    #5 footer mess

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <!-- <div class="footer-wrapper">
-	<footer class="content-info container-fluid" role="contentinfo">
+	<div class="content-info container-fluid" role="contentinfo">
 	  <div class="footer-one">
 	    <?php dynamic_sidebar('sidebar-footer-one'); ?>
 	  </div>
@@ -9,9 +9,9 @@
 	  <div class="footer-three">
 	    <?php dynamic_sidebar('sidebar-footer-three'); ?>
 	  </div>
-	</footer>
+	</div>
 </div> -->
 
-<div class="footer-end">
+<footer class="footer-end">
   	<p>2015 Simulacrum</p>
-</div>
+</footer>
