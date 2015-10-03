2a41029720e8375b4b1c7513dd0d7b97514ab6ae
Zaturrby <rjkorteschiel@gmail.com>
2015-10-03 11:16:44 +0200

    #10 again a fix for indentation

--- templates/nav-side.php
+++ templates/nav-side.php
@@ -4,9 +4,9 @@
       <div class="header-left">
         <nav class="navbar navbar-default" role="navigation">
           <?php
-            if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-            endif;
+          if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          endif;
           ?>
       	</nav>
       </div>
