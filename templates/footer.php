3ebe1592ca3a8d1d2b249dbc98af1b09bad7712a
Zaturrby <rjkorteschiel@gmail.com>
2015-10-03 21:02:28 +0200

    #5 overdid it with the overflow x on html

--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,3 @@
-<!-- <div class="footer-wrapper">
-	<div class="content-info container-fluid" role="contentinfo">
-	  <div class="footer-one">
-	    <?php dynamic_sidebar('sidebar-footer-one'); ?>
-	  </div>
-	  <div class="footer-two">
-	    <?php dynamic_sidebar('sidebar-footer-two'); ?>
-	  </div>
-	  <div class="footer-three">
-	    <?php dynamic_sidebar('sidebar-footer-three'); ?>
-	  </div>
-	</div>
-</div> -->
-
 <footer class="footer-end">
   	<p>2015 Simulacrum</p>
 </footer>
