74a4cccc70af84c06a90dd131f6ff6d66b88cdcf
Zaturrby <rjkorteschiel@gmail.com>
2015-10-04 11:03:36 +0200

    #8 changes on about page

--- templates/content-frontpage-about.php
+++ templates/content-frontpage-about.php
@@ -23,16 +23,76 @@
 		<h3>Redactie</h3>
 		<p>De redactie van Simulacrum bestaat uit gemotiveerde studenten en net-afgestudeerden die zich vrijwillig inzetten voor het tijdschrift. Maak hieronder kennis met onze redactieleden door op de foto’s te klikken.</p>
 		<p>Heb jij ook spannende ideeën voor de invulling van onze nummers, een passie voor kunst en taal en wil jij een netwerk opbouwen in de Amsterdamse kunstwereld? Stuur dan je CV en motivatie naar info@simulacrum.nl.</p>
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>		
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="single-member" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
+		<div class="single-member">
+			<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>		
+			<div class="member-text">
+				<h4>Member Name</h4>		
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam est laboriosam alias? Minus ullam quas eos ipsa fugiat eaque corrupti libero sit voluptatum odio, accusamus velit, minima vero esse itaque.</p>
+			</div>
+		</div>
 	</div>
 	<div class="about-retired-staff">
 		<h3>Oud redactie leden</h3>
