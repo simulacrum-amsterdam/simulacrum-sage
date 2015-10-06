59e24b386d563e0ff47c2d204c53b1e5b3d67a9b
Zaturrby <rjkorteschiel@gmail.com>
2015-10-06 13:43:59 +0200

    #7 postflow reasonable

--- templates/content-frontpage.php
+++ templates/content-frontpage.php
@@ -14,26 +14,6 @@
 	<div class="frontpage-current-issue">
 		<h5 class="small-title"> Nieuwste nummer: </h5>
 		<img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
-		<h1>OEF!</h1>
-		<p> Dit geweldige nieuwe nummer verkent Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa deserunt adipisci tenetur nostrum a libero, et fugit magnam cum. Sunt odio numquam quas earum beatae quaerat commodi, accusamus ducimus velit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nisi obcaecati molestiae dignissimos, assumenda nemo fuga tempore enim deleniti consequatur animi fugiat doloremque sit natus ullam adipisci perferendis nam incidunt.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis voluptatem, fuga voluptas velit fugiat modi iusto inventore! Eligendi recusandae optio tenetur, blanditiis, voluptatibus sed quas ut enim similique nostrum explicabo.</p>
-	</div>
-</div>
-<div class="frontpage-old-issues">
-	<h5 class="small-title"> Oudere nummers: </h5>
-	<div class="single-issue">
-		<img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
-		<div class="single-issue-text">
-			<h4>Let's get digital</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis voluptatem, fuga voluptas velit fugiat modi iusto inventore! Eligendi recusandae optio tenetur, blanditiis, voluptatibus sed quas ut enim similique nostrum explicabo.</p>
-		</div>
-	</div>
-	<div class="single-issue">
-		<img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
-		<div class="single-issue-text">
-			<h4>Waanzin</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis voluptatem, fuga voluptas velit fugiat modi iusto inventore! Eligendi recusandae optio tenetur, blanditiis, voluptatibus sed quas ut enim similique nostrum explicabo.</p>
-		</div>
 	</div>
 </div>
 
