f5135f83bd618da2bcc34122526c6c8422fb3321
Zaturrby <rjkorteschiel@gmail.com>
2015-10-06 12:43:21 +0200

    #10 travis style errors

--- templates/content-frontpage.php
+++ templates/content-frontpage.php
@@ -3,10 +3,10 @@
 <div class="frontpage-container">
 	<div class="post-container">
 		<?php $excludeCatID = get_cat_ID('bekendmakingen'); ?>
-		<?php $latest = new WP_Query( array( 
-			'showposts' => 10,	
-			'category__not_in' => $excludeCatID
-			)); ?>
+		<?php $latest = new WP_Query(array(
+            'showposts' => 10,
+            'category__not_in' => $excludeCatID
+        )); ?>
 		<?php while ($latest->have_posts()) : $latest->the_post(); ?>
 		<?php get_template_part('templates/content-frontpage-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 		<?php endwhile; ?>
