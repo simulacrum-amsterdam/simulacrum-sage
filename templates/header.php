ca1cb6599e0c1e16b3b16eee35f6bdfb23a5cc50
Zaturrby <rjkorteschiel@gmail.com>
2015-10-06 15:43:19 +0200

    #24 nav-top is a lot better

--- templates/header.php
+++ templates/header.php
@@ -4,9 +4,14 @@
 ?>
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
-		<ul class="nav navbar-nav">
+		<ul class="nav navbar-nav hidden-xs">
 			<li><a href="#">Portaal</a></li>
 			<li><a href="#">Archief</a></li>
+		</ul>
+		<div class="logo-container">
+	        <a href="<?= esc_url(home_url('/')); ?>"><img class="small-logo" src="<?php bloginfo('template_directory'); ?>/dist/images/simulacrum-logo-small2.svg"/></a>
+		</div>
+		<ul class="nav navbar-nav navbar-right hidden-xs">
 			<li><a href="#">Contact</a></li>
 			<li><a href="#">About</a></li>
 		</ul>
