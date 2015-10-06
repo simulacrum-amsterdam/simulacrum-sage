7e9b18d4643469363d4ece95701b74b632e8845f
Zaturrby <rjkorteschiel@gmail.com>
2015-10-06 13:19:05 +0200

    #7 frontpage layout

--- templates/content-frontpage-post.php
+++ templates/content-frontpage-post.php
@@ -1,8 +1,13 @@
-<article <?php post_class(); ?>>
-	<header>
-  	<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-	</header>
-	<div class="entry-summary">
-    	<?php echo the_excerpt(); ?>
+<?php $backgroundImageSrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+
+<article <?php post_class(); ?> style="background-image: url(<?php echo $backgroundImageSrc[0]; ?>);">
+    <div class="inner-post-container">
+    	<header>
+      	<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+    	</header>
+    	<div class="entry-summary">
+        	<?php echo the_excerpt(); ?>
+      </div>
   </div>
+
 </article>
