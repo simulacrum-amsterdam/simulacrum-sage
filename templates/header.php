13aaf6c6550add62814d0888575f56e202b45562
Zaturrby <rjkorteschiel@gmail.com>
2015-10-07 14:55:38 +0200

    #10 travis

--- templates/header.php
+++ templates/header.php
@@ -1,29 +1,30 @@
 <?php
     use Roots\Sage\Titles;
-    $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '');
+    $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), array(5600,1000), false, '');
 ?>
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
-		<?php
-		if (has_nav_menu('header_navigation-left')) :
-        	wp_nav_menu([
-        		'theme_location' => 'header_navigation-left', 
-        		'menu_class' => 'nav navbar-nav hidden-xs',
-        		'container' => false
-        	]);
-      	endif;
-      	?>
+        <?php
+        if (has_nav_menu('header_navigation-left')) :
+            wp_nav_menu([
+                'theme_location' => 'header_navigation-left', 
+                'menu_class' => 'nav navbar-nav hidden-xs',
+                'container' => false
+            ]);
+        endif;
+        ?>
 		<div class="logo-container">
 	        <a href="<?= esc_url(home_url('/')); ?>"><img class="small-logo" src="<?php bloginfo('template_directory'); ?>/dist/images/simulacrum-logo-small2.svg"/></a>
 		</div>
 		<?php
 		if (has_nav_menu('header_navigation-right')) :
-        	wp_nav_menu([
-        		'theme_location' => 'header_navigation-right', 
-        		'menu_class' => 'nav navbar-nav hidden-xs navbar-right',
-        		'container' => false
-             	]);
-      	endif;
-      	?>
+            wp_nav_menu([
+                'theme_location' => 'header_navigation-right', 
+                'menu_class' => 'nav navbar-nav hidden-xs navbar-right',
+                'container' => false
+            ]);
+        endif;
+        ?>
+
 	</div>
 </nav>
