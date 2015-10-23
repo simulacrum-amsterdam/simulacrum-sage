3233fb22487f0f702546799196e0f00587042ce3
Zaturrby <rjkorteschiel@gmail.com>
2015-10-23 10:52:27 +0200

    removed nav and excluded nummers category, so it can be used on top #24

--- templates/frontpage/content-frontpage.php
+++ templates/frontpage/content-frontpage.php
@@ -3,16 +3,14 @@
 <div class="frontpage-container">
 	<div class="post-container">
 		<?php $excludeCatID = get_cat_ID('bekendmakingen'); ?>
+		<?php $excludeCatIDTwo = get_cat_ID('nummers'); ?>
 		<?php $latest = new WP_Query(array(
             'showposts' => 10,
-            'category__not_in' => $excludeCatID
+            'category__not_in' => array($excludeCatID, $excludeCatIDTwo)
         )); ?>
 		<?php while ($latest->have_posts()) : $latest->the_post(); ?>
 		<?php get_template_part('templates/frontpage/content-frontpage-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 		<?php endwhile; ?>
-	</div>
-	<div class="frontpage-current-issue">
-		<img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 	</div>
 </div>
 
