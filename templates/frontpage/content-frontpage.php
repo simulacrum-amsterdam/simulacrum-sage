0dc06baa0c48d51203a8906b697ef605e0af01f6
Zaturrby <rjkorteschiel@gmail.com>
2015-10-23 10:28:29 +0200

    organizing templates #28

--- templates/frontpage/content-frontpage.php
+++ templates/frontpage/content-frontpage.php
@@ -8,7 +8,7 @@
             'category__not_in' => $excludeCatID
         )); ?>
 		<?php while ($latest->have_posts()) : $latest->the_post(); ?>
-		<?php get_template_part('templates/content-frontpage-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+		<?php get_template_part('templates/frontpage/content-frontpage-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 		<?php endwhile; ?>
 	</div>
 	<div class="frontpage-current-issue">
