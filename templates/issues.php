0273b639b2366dd585812bb151dd743ff3901cf4
Zaturrby <rjkorteschiel@gmail.com>
2015-10-25 14:39:52 +0100

    #24 working on the navigation-top and #8 about view

--- templates/issues.php
+++ templates/issues.php
@@ -1,4 +1,4 @@
-<div class="issue-membership-heading-container"><h4 class="issue-membership-header">Nummers:</h4></div>
+<div class="issue-membership-heading-container"><p>[ Nummers ]</p></div>
 
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
@@ -8,4 +8,4 @@
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 
-<div class="issue-membership-heading-container"><h4 class="issue-membership-header">Lid worden?</h4></div>
+<div class="issue-membership-heading-container lidworden"><p>[ Word lid! ]</p></div>
