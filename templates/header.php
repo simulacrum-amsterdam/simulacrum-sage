0d2d15cc0acf0182632bfb79ad5a78973a6134aa
Zaturrby <rjkorteschiel@gmail.com>
2015-10-26 14:53:42 +0100

    #24 overflow issue

--- templates/header.php
+++ templates/header.php
@@ -2,17 +2,17 @@
     use Roots\Sage\Titles;
     $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), array(5600,1000), false, '');
 ?>
-<nav class="navbar navbar-default">
-	<div class="container-fluid">
+<nav class="navigation">
         <?php
         if (has_nav_menu('header_navigation-left')) :
             wp_nav_menu([
                 'theme_location' => 'header_navigation-left', 
-                'menu_class' => 'nav navbar-nav hidden-xs',
+                'menu_class' => 'navigation-left hidden-xs',
                 'container' => false
             ]);
         endif;
         ?>
+
 		<div class="logo-container">
 	        <a href="<?= esc_url(home_url('/')); ?>"><img class="small-logo" src="<?php bloginfo('template_directory'); ?>/dist/images/simulacrum-logo-small2.svg"/></a>
 		</div>
@@ -20,13 +20,11 @@
 		if (has_nav_menu('header_navigation-right')) :
             wp_nav_menu([
                 'theme_location' => 'header_navigation-right', 
-                'menu_class' => 'nav navbar-nav hidden-xs navbar-right',
+                'menu_class' => 'navigation-right hidden-xs',
                 'container' => false
             ]);
         endif;
         ?>
-
-	</div>
 </nav>
 
     <?php if (Titles\title() == "frontpage" || Titles\title() == "frontpage-about") { 
--- templates/issues.php
+++ templates/issues.php
@@ -1,8 +1,7 @@
-
+<img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
+<span class="glyphicon glyphicon-menu-right call-for-papers-arrow" aria-hidden="true"></span>
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
-<img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
-<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
 <h1 class="large-background">NUMMERS</h1>
