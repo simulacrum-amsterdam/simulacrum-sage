de35d03963a2bf617b218831a83f4489fa09910c
Zaturrby <rjkorteschiel@gmail.com>
2015-10-26 10:44:37 +0100

    #24 change recente nummers naar nummers

--- templates/issues.php
+++ templates/issues.php
@@ -5,4 +5,4 @@
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 <img class="issue-image" src="<?php bloginfo('template_directory'); ?>/dist/images/Cover-OEF.jpg"/>
 <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-<h1 class="large-background">RECENTE NUMMERS</h1>
+<h1 class="large-background">NUMMERS</h1>
