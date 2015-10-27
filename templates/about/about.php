c8bea6cb302f3ebb9ab226987e22782b380f491e
Zaturrby <rjkorteschiel@gmail.com>
2015-10-27 18:31:45 +0100

    #8 templating from about page halfway

--- templates/about/about.php
+++ templates/about/about.php
@@ -1,21 +1,31 @@
 <?php use Roots\Sage\Titles; ?>
-
-<div class="about-news">
-	<?php $latest = new WP_Query('showposts=5&category_name=bekendmakingen'); ?>
-	<?php while ($latest->have_posts()) : $latest->the_post(); ?>
-	<?php get_template_part('templates/about/news-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-	<?php endwhile; ?>
-</div>
-
-<div class="about-top-row">
-	<div class="about">
+<?php $page = get_post($post->ID); ?>
+<div class="about-container">
+	<div class="about-news">
+		<?php $latest = new WP_Query('showposts=5&category_name=bekendmakingen'); ?>
+		<?php while ($latest->have_posts()) : $latest->the_post(); ?>
+		<?php get_template_part('templates/about/news-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+		<?php endwhile; ?>
+		<?php wp_reset_query(); ?>
+	</div>
+	<div class="about-text">
 		<h3>Over Simulacrum</h3>
-		<p> Simulacrum is een wetenschappelijk tijdschrift voor kunst en cultuur, dat dient als laagdrempelig en kwalitatief publicatieplatform voor studenten en deskundigen uit het veld. </p>
-		<p>	We brengen vier maal per jaar een nummer uit, iedere keer onder de noemer van een bepaald thema. De onderwerpen worden altijd benaderd vanuit verschillende disciplines binnen de kunst- en cultuurwetenschappen, en zowel in historisch als hedendaags perspectief geplaatst. De redactie bestaat uit studenten, veelal van de Universiteit van Amsterdam.</p> 
-		<p>Interesse? Zoek ons op in de boekhandel, word abonnee, schrijf een bijdrage of verrijk de redactie!</p>
+		<?= the_content();?>
+	</div>
+	<div class="about-people">
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>
+  		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>
 	</div>
 	<div class="about-topics">
-		<h3>Onderwerpen</h3>
+		<h3>Belangrijke onderwerpen</h3>
 		<?php
 		if (has_nav_menu('about_navigation')) :
             wp_nav_menu([
@@ -25,16 +35,17 @@
             ]);
         endif;
         ?>
-<!-- 		<h4>Call for papers</h4>
-		<h4>Gids voor auteurs</h4>
-		<h4>Wetenschappelijke process</h4>
-		<h4>Contact informatie</h4>
-		<h4>Advertentie beleid</h4>
-		<h4>Privacy beleid</h4>
-		<h4>Open Access / Licenties</h4> -->
 	</div>
 </div>
-<div class="about-people-row">
+
+
+
+
+
+<!-- <div class="about-top-row">
+	
+</div> -->
+<!-- <div class="about-people-row">
 	<div class="about-editorial">
 		<h3>Redactie</h3>
 		<p>De redactie van Simulacrum bestaat uit gemotiveerde studenten en net-afgestudeerden die zich vrijwillig inzetten voor het tijdschrift. Maak hieronder kennis met onze redactieleden door op de foto’s te klikken.</p>
@@ -46,19 +57,6 @@
 			<p>1012 XT Amsterdam</p>
 		</div>
 	</div>
-	<div class="about-staff">
-		<h3>Huidige Redactie</h3>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>
-		<img class="single-member" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>		
-		<img class="portret-current" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>		
-	</div>
-</div>
+</div> -->
 
 
