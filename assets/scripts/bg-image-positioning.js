418db4df985d49452f5e34af44ee2bd7b48c8391
Zaturrby <rjkorteschiel@gmail.com>
2015-11-07 22:07:22 +0100

    #8 iamge about bugfix

--- assets/scripts/bg-image-positioning.js
+++ assets/scripts/bg-image-positioning.js
@@ -2,6 +2,11 @@ function togglePost(event){
 	if (event) {event.stopPropagation();}
 	var postMargin = document.querySelector('.single_post').style.marginTop;
 	var bgImgHeight = jQuery('.background-image-wrapper').height();
+	if (bgImgHeight < 500){
+		document.querySelector('.single_post').style.marginTop = '25px';
+		return;
+	}
+
 	if (postMargin == '' || postMargin == '-75px') {
 		document.querySelector('.single_post').style.marginTop = (-bgImgHeight + 50) + 'px';
 		jQuery('.toggle-post > .glyphicon').detach();
@@ -13,25 +18,28 @@ function togglePost(event){
 	}
 }
 
+function toggleAbout(event) {
+	if (jQuery('.background-image').height() < 768){
+		var offset = 0;
+		document.querySelector('.about-container').style.marginTop = offset + 'px';	
+		return;
+	} else {
+		var offset = (-jQuery('.background-image').height() + 100);
+		document.querySelector('.about-container').style.marginTop = offset + 'px';	
+	}
+}
+
 var bgContainer = document.querySelector('.background-image-wrapper');
 imagesLoaded(bgContainer, function(){
 	// Setting of event listeners for post scrolling
 	if (document.querySelector('.toggle-post')) {
-		if (jQuery('.background-image').height() < 500){
-			return;
-		} else {
-			window.setTimeout(togglePost, 500);
-			jQuery('.background-image, .toggle-post').click(togglePost);
-		}
+		window.setTimeout(togglePost, 500);
+		jQuery('.background-image, .toggle-post').click(togglePost);
+		jQuery(window).resize(togglePost);
 	}
 	// Seperate handler for about
 	if (document.querySelector('.about-container')) {
-		if (jQuery('.background-image').height() < 500){
-			return;
-		} else {
-			var offset = (-jQuery('.background-image').height() + 100);
-			document.querySelector('.about-container').style.marginTop = offset + 'px';
-		}
+		toggleAbout();
+		jQuery(window).resize(toggleAbout);
 	}
-	
 });
