dab8fc6fec2776c12ef7515cdba60f59582bc1c5
Zaturrby <rjkorteschiel@gmail.com>
2015-11-09 18:59:17 +0100

    small changes frontpage #7

--- templates/frontpage/issues.php
+++ templates/frontpage/issues.php
@@ -4,8 +4,13 @@
     'cat' => $includeCatID
 )); ?>
 <?php while ($latest->have_posts()) : $latest->the_post(); ?>
-<?php get_template_part('templates/frontpage/issue', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+<?php // get_template_part('templates/frontpage/issue', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
+
+<div class="call-for-papers-text">
+	<h3>CALL FOR</h3>
+	<h3 class="call-papers">PAPERS</h3>
+</div>
 
 <span class="glyphicon glyphicon-menu-right call-for-papers-arrow" aria-hidden="true"></span>
 
