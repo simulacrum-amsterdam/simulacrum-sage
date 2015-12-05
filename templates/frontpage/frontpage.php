6eef1cff8669729fadd34855d5ee42cd5f1bc0a0
Zaturrby <rjkorteschiel@gmail.com>
2015-12-05 12:15:57 +0100

    #38 made header a bit more responsive, made pagination work

--- templates/frontpage/frontpage.php
+++ templates/frontpage/frontpage.php
@@ -3,7 +3,15 @@
 	<img class="simulacrum-logo" src="<?php bloginfo('template_directory'); ?>/dist/images/logo-sim-designers.svg" alt="">
 </div>
 <div>
-	<h1 class="simulacrum-issue">De Tourist</h1>		
+	<?php $includeCatID = get_cat_ID('nummers'); ?>
+	<?php $latest = new WP_Query(array(
+	    'showposts' => 1,
+	    'cat' => $includeCatID
+	)); 
+	?>
+	<?php while ($latest->have_posts()) : $latest->the_post(); ?>
+		<a href="<?= the_permalink(); ?>"><h1 class="simulacrum-issue"><?= the_title(); ?></h1></a>		
+	<?php endwhile; ?>
 </div>
 <div class="post-container">
 	<!-- Pages need to increase trough query var -->
@@ -15,11 +23,14 @@
 	<?php $excludeCatIDTwo = get_cat_ID('nummers'); ?>
 	<?php $excludeCatIDThree = get_cat_ID('call-for-papers'); ?>
 
-	<?php $latest = new WP_Query(array(
-        'posts_per_page' => 10,
-        'paged' => $paged,
-        'category__not_in' => array($excludeCatID, $excludeCatIDTwo, $excludeCatIDThree)
-    )); ?>
+	<?php 
+		$latest = new WP_Query(array(
+	        'posts_per_page' => 10,
+	        'paged' => $paged,
+	        'category__not_in' => array($excludeCatID, $excludeCatIDTwo, $excludeCatIDThree)
+	    ));
+	    $max_pages = $latest->max_num_pages;
+    ?>
 
 	<!-- Here the post template gets printed out -->
 	<?php while ($latest->have_posts()) : $latest->the_post(); ?>
@@ -29,10 +40,11 @@
 	<!-- Post links -->
 	<h6>
 		<?php if ($paged !== 1){ ?>
-			<a href="?page=1"><- Nieuwere posts</a>
+			<a href="?page=<?php echo $paged - 1; ?>"><- Nieuwere posts</a>
 		<?php } ?>
-
-		<a href="?page=2" ><span class="oudere-posts">Oudere posts -></span></a>
+		<?php if ($paged && $paged < $max_pages){ ?>
+			<a href="?page=<?php echo $paged + 1; ?> " ><span class="oudere-posts">Oudere posts -></span></a>
+		<?php } ?>
 	</h6>
 </div>
 
