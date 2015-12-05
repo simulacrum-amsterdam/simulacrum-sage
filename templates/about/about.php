4fe980019f0e79f90bad22f9a1c8de42a87ba334
Zaturrby <rjkorteschiel@gmail.com>
2015-12-05 14:30:15 +0100

    #38 about lots of styling

--- templates/about/about.php
+++ templates/about/about.php
@@ -9,49 +9,51 @@
 	  	</div>
 <?php } ?>
 
-<div class="about-news">
-	<?php 
-		function custom_excerpt_length($length) {
-		  return 20;
-		}
+<div class="about-container">
+	<div class="about-description">
+		<h3><?= the_title(); ?></h3>
+		<?= the_content(); ?>
+	</div>
+	<div class="about-news">
+		<h3> Nieuws </h3>
 
-		add_filter('excerpt_length', 'custom_excerpt_length', 999);
-	?>
-	<?php $latest = new WP_Query('showposts=5&category_name=bekendmakingen'); ?>
-	<?php while ($latest->have_posts()) : $latest->the_post(); ?>
-	<?php get_template_part('templates/about/news-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-	<?php endwhile; ?>
-	<?php wp_reset_query(); ?>
+		<?php 
+			function custom_excerpt_length($length) {
+			  return 20;
+			}
+
+			add_filter('excerpt_length', 'custom_excerpt_length', 999);
+		?>
+		<?php $latest = new WP_Query('showposts=5&category_name=bekendmakingen'); ?>
+		<?php while ($latest->have_posts()) : $latest->the_post(); ?>
+		<?php get_template_part('templates/about/news-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+		<?php endwhile; ?>
+		<?php wp_reset_query(); ?>
+	</div>
 </div>
-<div class="about-text">
-	<h3><?= the_title(); ?></h3>
-	<?= the_content(); ?>
-</div>
+
 <div class="about-people">
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>
-		<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>
+	<h3> Redactie </h3>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>
+	<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>
 </div>
 
-
-
-
-
-<!-- <div class="about-top-row">
-	
-</div> -->
-<!-- <div class="about-people-row">
-	<div class="about-editorial">
-		<h3>Redactie</h3>
+<div class="about-bottom-container">
+	<div class="about-write">
+		<h3> Schrijven </h3>
 		<p>De redactie van Simulacrum bestaat uit gemotiveerde studenten en net-afgestudeerden die zich vrijwillig inzetten voor het tijdschrift. Maak hieronder kennis met onze redactieleden door op de foto’s te klikken.</p>
 		<p>Heb jij ook spannende ideeën voor de invulling van onze nummers, een passie voor kunst en taal en wil jij een netwerk opbouwen in de Amsterdamse kunstwereld? Stuur dan je CV en motivatie naar info@simulacrum.nl.</p>
+	</div>
+	<div class="about-ads">
+		<h3>Contact</h3>
 		<div class="contact-gegevens">
 			<h4>Postadres</h4>
 			<p>Stichting Simulacrum</p>
@@ -59,6 +61,4 @@
 			<p>1012 XT Amsterdam</p>
 		</div>
 	</div>
-</div> -->
-
-
+</div>
