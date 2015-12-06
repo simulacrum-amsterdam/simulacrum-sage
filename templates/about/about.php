cc1f845e9a3a494726c6ccdaa1a12bf919f6c1db
Zaturrby <rjkorteschiel@gmail.com>
2015-12-06 07:55:00 +0100

    #40 about messy but with BEM methodology

--- templates/about/about.php
+++ templates/about/about.php
@@ -1,6 +1,3 @@
-<?php use Roots\Sage\Titles; ?>
-<?php $page = get_post($post->ID); ?>
-
 <?php
 	$backgroundImageSrc = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
 	if (  has_post_thumbnail() == 1 && !has_tag('no-background', $post->ID)) { ?> 
@@ -10,47 +7,44 @@
 <?php } ?>
 
 <div class="about-container">
-	<div class="about-description">
-		<h3><?= the_title(); ?></h3>
-		<?= the_content(); ?>
-		<div class="about-people">
+	<div class="about-left">
+		<div class="about-left__description">
+			<h3><?= the_title(); ?></h3>
+			<?= the_content(); ?>
+		</div>
+		<div class="about-left__people">
 			<h3> Redactie </h3>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>
-			<img src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-anne.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-franziska.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-frederike.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-julia.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-lindy.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-maartje.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-mylene.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-querine.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofia.jpg"/>
+			<img class="person" src="<?php bloginfo('template_directory'); ?>/dist/images/fotos/portret-sofie.jpg"/>
 		</div>
 	</div>
-	<div class="about-news">
-		<h3> Nieuws </h3>
+	<div class="about-right"></div>
+		<div class="about-right__news">
+			<h3> Nieuws </h3>
+			<?php 
+				function custom_excerpt_length($length) {
+				  return 20;
+				}
 
-		<?php 
-			function custom_excerpt_length($length) {
-			  return 20;
-			}
-
-			add_filter('excerpt_length', 'custom_excerpt_length', 999);
-		?>
-		<?php $latest = new WP_Query('showposts=3&category_name=bekendmakingen'); ?>
-		<?php while ($latest->have_posts()) : $latest->the_post(); ?>
-		<?php get_template_part('templates/about/news-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-		<?php endwhile; ?>
-		<?php wp_reset_query(); ?>
-		<div class="about-write">
-			<?= get_post_custom_values('second_page_header')[0]; ?>
+				add_filter('excerpt_length', 'custom_excerpt_length', 999);
+			?>
+			<?php $latest = new WP_Query('showposts=3&category_name=bekendmakingen'); ?>
+			<?php while ($latest->have_posts()) : $latest->the_post(); ?>
+			<?php get_template_part('templates/about/news-post', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+			<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+		</div>
+		<div class="about-right__write">
+			<h3><?= get_post_custom_values('second_page_header')[0]; ?></h3>
 			<?= get_post_custom_values('second_page_content')[0]; ?>
-
 		</div>
 	</div>
 </div>
-<div class="about-people-container">
-	
-
-</div>
-	
