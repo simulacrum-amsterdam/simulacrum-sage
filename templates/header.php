d7faeeb395e5dbf94280b272320463895806cad3
Zaturrby <rjkorteschiel@gmail.com>
2015-12-06 17:37:02 +0100

    #44 responsive nav fix

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
     $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), array(5600,1000), false, '');
 ?>
 <nav class="navigation">
-    <div class="nav-item">
+    <div class="nav__item nav__item-left">
         <?php
         if (has_nav_menu('header_navigation-left')) :
             wp_nav_menu([
@@ -14,7 +14,7 @@
         endif;
         ?>
     </div>
-	<div class="nav-item"> 
+	<div class="nav__item nav__item-right"> 
         <?php
 		if (has_nav_menu('header_navigation-right')) :
             wp_nav_menu([
