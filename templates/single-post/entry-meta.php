15122c758ca4b458afa4267995ffe2d56baba3b5
Zaturrby <rjkorteschiel@gmail.com>
2015-12-28 18:48:45 +0100

    add some form stuff

--- templates/single-post/entry-meta.php
+++ templates/single-post/entry-meta.php
@@ -1,5 +1,5 @@
 <div class="entry-meta">
-    <?php if(!has_tag('no-author', $post->ID)){ ?>
-	<p class="byline author vcard"><?= get_the_author(); ?></p>
+  <?php if(!has_tag('no-author', $post->ID)){ ?>
+  	<p class="byline author vcard"><?= get_the_author(); ?></p>
 	<?php } ?>
 </div>
