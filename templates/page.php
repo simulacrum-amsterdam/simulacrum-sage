dc8daf639c243b43bd5db996a8d725f2dddc964b
Zaturrby <rjkorteschiel@gmail.com>
2016-03-22 11:43:42 +0100

    checkbox

--- templates/page.php
+++ templates/page.php
@@ -1,4 +1,4 @@
 <?php get_template_part('templates/header-feature'); ?>
 <main class="sc-content">
-  <?php the_content(); ?>
+  <div class="sc-content__content-container"><?php the_content(); ?></div> 
 </main>
