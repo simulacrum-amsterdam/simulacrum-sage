e7958a4659c7d5354098b9c3208d5e893baa7466
Zaturrby <rjkorteschiel@gmail.com>
2016-03-22 11:13:38 +0100

    working with checkboxes

--- templates/excerpt-fw.php
+++ templates/excerpt-fw.php
@@ -1,15 +1,15 @@
 <?php $backgroundImageSrc = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');?>
 
-<article <?php post_class('fp-post'); ?> >
+<article <?php post_class('excerpt-fw'); ?> >
   <a href="<?php the_permalink(); ?>">
-  	<div class="fp-post__image-container">
-  		<img class="fp-post__image"src="<?php echo $backgroundImageSrc[0]; ?>">
+  	<div class="excerpt-fw__image-container">
+  		<img class="excerpt-fw__image"src="<?php echo $backgroundImageSrc[0]; ?>">
   	</div>
   </a>
-  <div class="fp-post__meta">
+  <div class="excerpt-fw__meta">
 		<a href="<?php the_permalink(); ?>">
-      <h3 class="fp-post__title"><?php the_title(); ?></h3>
+      <h3 class="excerpt-fw__title"><?php the_title(); ?></h3>
     </a>
-		<h6 class="fp-post__author"><?= get_the_author(); ?></h6>
+		<h6 class="excerpt-fw__author"><?= get_the_author(); ?></h6>
   </div>
 </article>
