e1dd19a19ec4a3c1bc2e2e7ece3a95596774eb51
Zaturrby <rjkorteschiel@gmail.com>
2016-03-26 11:43:03 +0100

    fix 404

--- 404.php
+++ 404.php
@@ -1,7 +1,9 @@
-<div class="page__container">
-
-  <div class="page-404">
-    <h2>Helaas, deze pagina bestaat niet</h2>
+<header class="layout__header" >
+  <?php get_template_part('templates/header'); ?>
+</header>
+<main class="layout__main">
+  <div class="sc">
+    <h1 style="text-align: center;">[404]</h1>
+    <h2 style="text-align: center;">Helaas, deze pagina bestaat niet</h2>
   </div>
-
-</div>
+</main>
