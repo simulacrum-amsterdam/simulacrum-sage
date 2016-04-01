c52dc83631453d3536e650ec2dff741a6047742d
Zaturrby <rjkorteschiel@gmail.com>
2016-04-01 16:55:36 +0200

    excerpt news looks like something - issue's page screwed up

--- templates/excerpt-news.php
+++ templates/excerpt-news.php
@@ -1,22 +1,38 @@
 <article <?php post_class('blocks-layout__block'); ?>>
   <header>
     <a href="<?php the_permalink(); ?>">  
-      <h4 class="excerpt-news__title"><?php the_title(); ?></h4>
-        <?php
-        $backgroundImageSrc = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
-        if (has_post_thumbnail() == 1 && !has_tag('no-background', $post->ID)) {
-        ?>
-        <div 
-        class="sc__image"
-        style="background-image: url( <?php echo $backgroundImageSrc[0]; ?> );">
-        </div>
-        <?php } ?>
+      <h4 class="blocks-layout__title"><?php the_title(); ?></h4>
     </a>
+    <?php
+    $backgroundImageSrc = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
+    if (has_post_thumbnail() == 1 && !has_tag('no-background', $post->ID)) {
+    ?>
+    <div 
+    class="blocks-layout__image"
+    style="background-image: url( <?php echo $backgroundImageSrc[0]; ?> );">
+      <div class="blocks-layout__summery">
+        <?php echo the_excerpt(); ?>
+      </div>
+    </div>
+    <?php } else { ?>
+      <div class="blocks-layout__summery-unhover">
+        <?php echo the_excerpt(); ?>
+      </div>
+    <?php } ?>
   </header>
-  <div class="excerpt-news__summery">
-    <?php echo the_excerpt(); ?>
-  </div>
-  <footer class="excerpt-news__footer">
-    <time class="excerpt-news__time" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+ 
+  <footer class="blocks-layout__footer">
+    <time class="blocks-layout__time" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
   </footer>
 </article>
+<?php
+  $random = rand(1,100);
+  if ($random > 20) {
+?>
+
+<div class="blocks-layout__block">
+</div>
+
+<?php
+  }
+?>
