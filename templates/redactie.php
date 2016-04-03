fcf0d6dc43365fc463f0b50d1d88ae233a1b3d57
Zaturrby <rjkorteschiel@gmail.com>
2016-04-03 12:54:30 +0200

    redaction

--- templates/redactie.php
+++ templates/redactie.php
@@ -1,49 +1,62 @@
+<?php
+function limit_text($text, $limit) {
+      if (str_word_count($text, 0) > $limit) {
+          $words = str_word_count($text, 2);
+          $pos = array_keys($words);
+          $text = substr($text, 0, $pos[$limit]) . '...';
+      }
+      return $text;
+    }
+?>
+
 <div class="sc">
-	<h1 class="sc__title">Redactie</h1>
-	<div class="redactie__container">
-		<?php global $wpdb;
-        $blog_id = get_current_blog_id();
+    <h1 class="sc__title">Redactie</h1>
+</div>
 
-        $wp_user_query = new WP_User_Query(
-            array(
-            'meta_query' => array(
-            'relation' => 'OR',
-            array(
-            'key' => $wpdb->get_blog_prefix($blog_id) . 'capabilities',
-            'value' => 'author',
-            'compare' => 'like'
-            ),
-            array(
-            'key' => $wpdb->get_blog_prefix($blog_id) . 'capabilities',
-            'value' => 'editor',
-            'compare' => 'like'
-            )
-            )
-            )
-        );
+<div class="redactie__container">
+	<?php global $wpdb;
+    $blog_id = get_current_blog_id();
 
-        $authors = $wp_user_query->get_results();
-        if (!empty($authors)) {
-          foreach ($authors as $author) {
-            $author_info = get_userdata($author->ID);
-            $attachment_id = get_user_meta( $author->ID, 'avatar_manager_custom_avatar', true ); 
-            $custom_avatar = get_post_meta( $attachment_id, '_avatar_manager_custom_avatar', true );
-            $avatar_url = avatar_manager_generate_avatar_url( $attachment_id, 300 );
-            ?>
-            <div class="redactie__member">
-            <div class="redactie__img-container">
-              <div class="redactie__img" style="background-image:url(<?php echo $avatar_url ?>);"> 
-                <div class="redactie__member-meta">
+    $wp_user_query = new WP_User_Query(
+        array(
+        'meta_query' => array(
+        'relation' => 'OR',
+        array(
+        'key' => $wpdb->get_blog_prefix($blog_id) . 'capabilities',
+        'value' => 'author',
+        'compare' => 'like'
+        ),
+        array(
+        'key' => $wpdb->get_blog_prefix($blog_id) . 'capabilities',
+        'value' => 'editor',
+        'compare' => 'like'
+        )
+        )
+        )
+    );
+
+    $authors = $wp_user_query->get_results();
+    if (!empty($authors)) {
+      foreach ($authors as $author) {
+        $author_info = get_userdata($author->ID);
+        $attachment_id = get_user_meta( $author->ID, 'avatar_manager_custom_avatar', true ); 
+        $custom_avatar = get_post_meta( $attachment_id, '_avatar_manager_custom_avatar', true );
+        $avatar_url = avatar_manager_generate_avatar_url( $attachment_id, 300 );
+        ?>
+        <div class="redactie__member">
+            <div class="redactie__member-meta">
+                <div>
                     <h3 class="redactie__member-title"><?php echo $author->display_name; ?></h3>
-                    <p class="redactie__member-summery"><?php echo $author->description; ?></p>
+                    <p class="redactie__member-summery">
+                    <?php echo limit_text($author->description, 35); ?>
+                    </p>
                 </div>
-              </div>
             </div>
-          </div>
-        <?php
-        }
-        } else {
-            echo 'No authors found';
-        }?>
-	</div>
+            <img class="redactie__img" src="<?php echo $avatar_url ?>"/>
+      </div>
+    <?php
+    }
+    } else {
+        echo 'No authors found';
+    }?>
 </div>
